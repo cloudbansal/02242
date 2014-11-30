b84973ede3e362c71870ae3a4cd60148dc5f2690
Alberto Rico Simal <alberto@echo.it>
2014-11-30 17:20:15 +0100

    Structures for RD and DS

---
+++ eclipse_project/src/dk/dtu/imm/pa/analyzer/objects/ReachingDefinitionsSet.java
@@ -0,0 +1,12 @@
+package dk.dtu.imm.pa.analyzer.objects;
+
+import java.util.ArrayList;
+
+public class ReachingDefinitionsSet extends ArrayList<ReachingDefinition> {
+
+	/**
+	 * Default variable for serialization
+	 */
+	private static final long serialVersionUID = 1L;
+	
+}
